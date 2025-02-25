a1fd0bd43f863d7b15419871b21c83db18c708dd
Dmitriy Safronov <zimniy@cyberbrain.pw>
2025-02-25 16:16:14 +0400

    Initial commit

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+git \
+  clone \
+  https://github.com/dmitriysafronov/ansible_role-template.git \
+  .update
+
+LATEST_TAG="$(git -C .update tag | grep -e '^v' | sort -V | tail -n 1)"
+
+git -C .update -c advice.detachedHead=false checkout ${LATEST_TAG}
+
+if [[ -s .update/update.list ]]; then
+  rsync -av \
+    --include-from=.update/update.list \
+    --exclude=* \
+    --delete-after \
+    .update/ \
+    ./
+fi
+
+rm -rf .update/
